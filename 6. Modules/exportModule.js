4268e6f56c2b206c6cd76ed68292726dfd93ba18
B-got-banned <onyealilachib0850@student.babcock.edu.ng>
2025-09-24 00:44:11 +0100

    Made more changes for illustration's sake

---
+++ 6. Modules/exportModule.js
@@ -0,0 +1,2 @@
+//modules allow for the import and export of code between files
+export const myCube = (num) => num * num * num//export this function myCube
---
+++ 6. Modules/index.js
@@ -0,0 +1,2 @@
+import {myCube} from "./exportModule.js" //import myCube function from relative module exportModule.js
+console.log(myCube(20))
